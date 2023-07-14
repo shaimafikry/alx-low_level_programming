f3ca5cdaa3a88a419b0d05ed083cc27662248daf
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-15 02:19:27 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,21 +17,20 @@ int main(void)
 	int y;
 	int z;
 
-	for (n = 48 ; n <=57 ; n++)
+	for (n = 48 ; n <= 57 ; n++)
 	{
-	for (x = n+1 ; x < 57 ; x++)
+	for (x = n + 1 ; x < 57 ; x++)
 	{
 	for (y = x + 1 ; y <= 57 ; y++)
 	{
 	for (z = y + 1 ; z <= 57 ; z++)
-	{	
-	
+	{
 		putchar (n);
 		putchar (x);
 		putchar (' ');
 		putchar (y);
 		putchar (z);
-		if ( n == 57 && x == 56 && y == 57 && z == 57 )
+		if (n == 57 && x == 56 && y == 57 && z == 57)
 		{
 			break;
 		}
