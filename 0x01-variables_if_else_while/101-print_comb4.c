288a54808f47df4c2c27f1a928545185e71d3051
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-15 02:00:14 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,7 +5,7 @@
 /**
 *main - Entry point
 *
-*Description: 'prints single numbers'
+*Description: 'prints two combination numbers'
 *
 *Return: Always 0 (Success)
 */
@@ -13,14 +13,33 @@
 int main(void)
 {
 	int n;
+	int x;
+	int y;
 
-	for (n = 48 ; n => 57 ; n++)
+	for (n = 48 ; n < 56 ; n++)
 	{
+	for (x = n + 1 ; x < 57 ; x++)
+	{
+	for (y = x + 1 ; y <= 57 ; y++)
+	{
+		if (x != n)
+		{
+		if (y != x)
+		{
 		putchar (n);
-		putchar (',')
+		putchar (x);
+		putchar (y);	
+		if (n == 55 && x == 56 && y == 57)
+		{
+			break;
+		}
+		putchar (',');
 		putchar (' ');
+		}
+		}
+	}
+	}
 	}
 	putchar ('\n');
 	return (0);
 }
-
