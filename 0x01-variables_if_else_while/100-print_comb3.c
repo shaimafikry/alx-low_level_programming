11c70503605d6a7d8b797a51c5c90b049d35ac02
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-15 01:48:59 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,9 +15,9 @@ int main(void)
 	int n;
 	int x;
 
-	for (n = 48 ; n => 57 ; n++)
+	for (n = 48 ; n < 57 ; n++)
 	{
-	for (x = n+1 ; x < 57 ; x++)
+	for (x = n+1 ; x <= 57 ; x++)
 	{
 		if (x != n)
 		{
