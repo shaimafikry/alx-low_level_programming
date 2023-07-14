7b54db3d2108212f5fbe42a537c88b328f28859f
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-15 02:17:15 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,7 +5,7 @@
 /**
 *main - Entry point
 *
-*Description: 'prints single numbers'
+*Description: 'prints two combination numbers'
 *
 *Return: Always 0 (Success)
 */
@@ -13,14 +13,34 @@
 int main(void)
 {
 	int n;
+	int x;
+	int y;
+	int z;
 
-	for (n = 48 ; n => 57 ; n++)
+	for (n = 48 ; n <=57 ; n++)
 	{
+	for (x = n+1 ; x < 57 ; x++)
+	{
+	for (y = x + 1 ; y <= 57 ; y++)
+	{
+	for (z = y + 1 ; z <= 57 ; z++)
+	{	
+	
 		putchar (n);
-		putchar (',')
+		putchar (x);
 		putchar (' ');
+		putchar (y);
+		putchar (z);
+		if ( n == 57 && x == 56 && y == 57 & z == 57 )
+		{
+			break;
+		}
+		putchar (',');
+		putchar (' ');
+	}
+	}
+	}
 	}
 	putchar ('\n');
 	return (0);
 }
-
