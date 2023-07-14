16c6a86d7a38129234f580e9703da3db658c6ac1
shimaa <shaimafikry@yahoo.com>
2023-07-14 15:36:31 -0700

    add files more

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: 'prints single numbers'
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n => 57 ; n++)
+	{
+		putchar (n);
+		putchar (',')
+		putchar (' ');
+	}
+	putchar ('\n');
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: 'prints lowercase alphapet in reverse'
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char n;
+
+	for (n = '48' ; n => '57' ; n++)
+	{
+		putchar (n);
+	}
+	for (n = 97 ; n <= 102 ; n++)
+	{
+		putchar (n);
+	}
+	putchar ('\n');
+	
+	return (0);
+}
+
