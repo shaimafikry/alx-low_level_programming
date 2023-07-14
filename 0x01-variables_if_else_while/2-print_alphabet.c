6de5f0909b343bee97fbe34164650a669dfc1e5a
shimaa <shaimafikry@yahoo.com>
2023-07-14 14:56:41 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: 'prints lowercase alphapet'
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 'a' ; n <= 'z' ; n++)
+	{
+		putchar(n);
+	}
+	return (0);
+}
+
