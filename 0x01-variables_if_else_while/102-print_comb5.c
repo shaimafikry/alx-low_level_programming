caaf3371052d412b40dafcb38203d43c9dfd8278
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-15 09:30:19 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -25,8 +25,6 @@ int main(void)
 	{
 	for (z = 48 ; z <= 57 ; z++)
 	{
-	if (n == y && x ==z)
-	{
 	if (n == y && x == z)
 	{
 	if (y != n && z != x)
@@ -42,7 +40,6 @@ int main(void)
 		}
 		putchar (',');
 		putchar (' ');
-	}
 	}
 	}
 	}
