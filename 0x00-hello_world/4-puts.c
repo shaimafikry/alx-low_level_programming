345406eeab08db66501e08f0379a3ce765759b24
shimaa <shaimafikry@yahoo.com>
2023-07-22 00:02:22 -0700

    edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(void){
-puts ("\"Programming is like building a multilingual puzzle");
-return 0;
-  }
+/*description
+ * how to use puts
+ */
+
+int main (void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
+
