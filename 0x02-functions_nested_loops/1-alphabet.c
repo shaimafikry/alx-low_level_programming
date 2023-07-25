dac6e1a589dad67f20c4d692a2318b208d47f04e
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-25 10:50:27 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,19 @@
-#!/bin/bash
+#include "main.h"
+
+/**
+* main entry point
+* print lower case alphapet
+* return 0 always success
+**/
+
+int main(void)
+{
+int n;
+
+for (n = 97 ; n <= 122 ; n++)
+{
+	_putchar(n);
+}
+_putchar('\n');
+return 0;
+}
