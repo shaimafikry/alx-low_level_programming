3c5d880c0baa06dfe9f8aef7edd868783814075b
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-26 17:43:59 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,33 +10,33 @@ void print_to_98(int n)
 {
 	if (n == 98)
 	{
-		printf("%d",n);
+		printf("%d", n);
 	}
-	if (n <= 98)
+	else if (n < 98)
 	{
 		for  (n = n ; n <= 98 ; n++)
 		{
 			if (n == 98)
 			{
-				printf("%d",n);
+				printf("%d", n);
 			}
 			else
 			{
-				printf("%d, ",n);
+				printf("%d, ", n);
 			}
 		}
 	}
-	if (n >= 98)
+	else (n > 98)
 	{
 		for  (n = n ; n >= 98 ; n--)
 		{
 			if (n == 98)
 			{
-				printf("%d",n);
+				printf("%d", n);
 			}
 			else
 			{
-				printf("%d, ",n);
+				printf("%d, ", n);
 			}
 		}
 	}
