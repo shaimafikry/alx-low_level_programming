6667bd61872344a930a9e8f43785f76954d0bcd4
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-26 16:07:37 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include "main.h"
+/**
+ * print_last_digit - checks numbers and gives abslute value
+ * @n : integer
+ * Return: vale of last digit
+ */
+int print_last_digit (int num)
+{
+	int last_d;
+
+	last_d = num % 10;
+	return (last_d); 
+}
