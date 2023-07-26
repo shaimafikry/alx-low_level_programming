bb03d6593c0adca0764a1d05e6351cd3ed6b69d1
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-26 16:02:32 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - checks numbers and print sign
  * @n : integer
- * Return: 
+ * Return:
  * 1 if n is >0
  * -1 if n is < 0
  * 0 if n is 0
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-                _putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n < 0)
 	{
-                _putchar('-');
+		_putchar('-');
 		return (-1);
 	}
-  else
-  {
-    _putchar('0');
-    return (0);
-  }
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
