aff92079baf83300de4016b12ca909219eea5453
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-27 00:26:34 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,7 +17,7 @@ void jack_bauer(void)
 		{
 		for (h2 = '0'; h2 <= '9'; h2++)
 			{
-		for (m = '0'; m <= ''; m++)
+		for (m = '0'; m <= '5'; m++)
 		{
 			for (m2 = '0'; m2 <= '9'; m2++)
 			{
@@ -36,7 +36,7 @@ void jack_bauer(void)
 		{
 		for (h2 = '0'; h2 <= '3'; h2++)
 			{
-		for (m = '0'; m <= ''; m++)
+		for (m = '0'; m <= '5'; m++)
 		{
 			for (m2 = '0'; m2 <= '9'; m2++)
 			{
