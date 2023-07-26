dc319e3f311645c33b55283e2c02eb8f2543c17b
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-26 10:15:50 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,13 +3,4 @@
 *for my c files c in alx
 **/
 char _putchar(char c);
-void print_alphabet(void)
-{
-	int n ;
-	
-	for (n = 97 ; n <= 122 ; n++)
-	{
-		_putchar(n);
-	}
-	_putchar('\n');
-}
+void print_alphabet(void);
