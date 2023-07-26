04cf57ad0038f152cd4b19da108ec2201b7ab303
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-27 02:38:45 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
 * main - entry point
 * sum of multiples of 3 & 5  below 1024
 * Return: 0 Always success
@@ -11,10 +11,10 @@ int main(void)
 
 	for (i = 0; i < 1024; i++)
 	{
-			if (i % 3 == 0 || i % 5 == 0)
-			{
-				x = x + i;
-			}
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			x = x + i;
+		}
 	}
 	printf("%d\n", x);
 
