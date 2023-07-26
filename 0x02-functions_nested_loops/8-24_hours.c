adf502ee6119b1b0c8c214569143e6dcc621cb9f
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-27 00:52:59 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * jack_bauer - prints every minute of the day
- * no input
  * Return: no return value
  */
 void jack_bauer(void)
@@ -13,25 +12,22 @@ void jack_bauer(void)
 
 	for (h = '0'; h <= '2'; h++)
 	{
-		if ( h != '2')
+		for (h2 = '0'; h2 <= '9'; h2++)
 		{
-			for (h2 = '0'; h2 <= '9'; h2++)
+			for (m = '0'; m <= '5'; m++)
 			{
-				for (m = '0'; m <= '5'; m++)
+				for (m2 = '0'; m2 <= '9'; m2++)
 				{
-					for (m2 = '0'; m2 <= '9'; m2++)
-					{
-						_putchar(h);
-						_putchar(h2);
-						_putchar(':');
-						_putchar(m);
-						_putchar(m2);
-						_putchar('\n');
-					}
+					_putchar(h);
+					_putchar(h2);
+					_putchar(':');
+					_putchar(m);
+					_putchar(m2);
+					_putchar('\n');
 				}
 			}
 		}
-		else
+		if (h != '2')
 		{
 			for (h2 = '0'; h2 <= '3'; h2++)
 			{
@@ -45,7 +41,6 @@ void jack_bauer(void)
 						_putchar(m);
 						_putchar(m2);
 						_putchar('\n');
-				
 					}
 				}
 			}
