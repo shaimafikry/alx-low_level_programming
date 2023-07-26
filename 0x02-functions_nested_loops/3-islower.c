b41c90d59445a43ae6e3f53076c46df0fc56277a
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-26 10:50:20 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,20 @@
-#!/bin/bash
+#include "main.h"
+/**
+ * _islower() - checks lowercase alphabet
+ * @c : integer
+ * Return : 1 if true
+ *		0 if false
+ **/
+int _islower(c)
+{
+	int c;
+
+	if (c >= 97 && <= 122)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
