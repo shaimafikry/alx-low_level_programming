f7faf9b99e022b45271e0a8b35c0532a1c247812
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-26 17:33:20 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,27 @@
-#!/bin/bash
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural num up to 98
+ * @n : integer
+ * Return: no return func
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for  (n ; n <= 98 ; n++ )
+		{
+			printf("%d, ",n);
+		}
+	}
+	if (n >= 98)
+	{
+		for  (n ; n >= 98 ; n-- )
+		{
+			printf("%d, ",n);
+		}
+	}
+	_putchar('\n');
+
+}
