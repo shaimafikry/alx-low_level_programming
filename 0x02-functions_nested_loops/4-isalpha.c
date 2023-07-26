4106156c098b49651b6bd3dc737e1c955db07802
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-26 14:45:25 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,23 @@
-#!/bin/bash
+#include "main.h"
+/**
+ * _isalpha() - checks  alphabet
+ * @c : integer
+ * Return : 1 if the input is alphabet
+ *		0 if it is not n aplphabet
+ **/
+int _isalpha(int c)
+{
+  
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+  else
+  {
+    return (0);
+  }
+}
