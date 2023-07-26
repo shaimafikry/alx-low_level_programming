7fddb9bf2f6c6094315a2a22ebd476b6607a3a84
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-27 00:49:16 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,41 +15,40 @@ void jack_bauer(void)
 	{
 		if ( h != '2')
 		{
-		for (h2 = '0'; h2 <= '9'; h2++)
+			for (h2 = '0'; h2 <= '9'; h2++)
 			{
-		for (m = '0'; m <= '5'; m++)
-		{
-			for (m2 = '0'; m2 <= '9'; m2++)
-			{
-					_putchar(h);
-					_putchar(h2);
-					_putchar(':');
-					_putchar(m);
-					_putchar(m2);
-					_putchar('\n');
-				
+				for (m = '0'; m <= '5'; m++)
+				{
+					for (m2 = '0'; m2 <= '9'; m2++)
+					{
+						_putchar(h);
+						_putchar(h2);
+						_putchar(':');
+						_putchar(m);
+						_putchar(m2);
+						_putchar('\n');
+					}
 				}
+			}
 		}
-	}
-	}
 		else
 		{
-		for (h2 = '0'; h2 <= '3'; h2++)
+			for (h2 = '0'; h2 <= '3'; h2++)
 			{
-		for (m = '0'; m <= '5'; m++)
-		{
-			for (m2 = '0'; m2 <= '9'; m2++)
-			{
-					_putchar(h);
-					_putchar(h2);
-					_putchar(':');
-					_putchar(m);
-					_putchar(m2);
-					_putchar('\n');
+				for (m = '0'; m <= '5'; m++)
+				{
+					for (m2 = '0'; m2 <= '9'; m2++)
+					{
+						_putchar(h);
+						_putchar(h2);
+						_putchar(':');
+						_putchar(m);
+						_putchar(m2);
+						_putchar('\n');
 				
+					}
 				}
+			}
 		}
 	}
-		}
-}
 }
