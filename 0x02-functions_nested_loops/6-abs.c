56ae39c8aa5057197599f5bbfc521608b05e28c9
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-26 16:04:37 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,23 @@
-#!/bin/bash
+#include "main.h"
+/**
+ * _abs - checks numbers and gives abslute value
+ * @n : integer
+ * Return: absluate value of a number
+ */
+int _abs(int)
+{	
+	int n;
+
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-1*n);
+	}
+	else
+	{
+		return (0);
+	}
+}
