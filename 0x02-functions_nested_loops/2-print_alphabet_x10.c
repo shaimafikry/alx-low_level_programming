4b6f3982bb7b55b16b0efad9fe1a03663b9230d8
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-26 10:42:09 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,20 @@
-#!/bin/bash
+#include "main.h"
+/*
+ * print_alphabet_x10() - prints lowercase alphabet
+ * no arguments
+ * Return :no return value
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	int n;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (n = 97; n <= 122; n++)
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
