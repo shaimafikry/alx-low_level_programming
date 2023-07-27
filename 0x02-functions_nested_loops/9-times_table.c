862a4b7f90eca539dfe565ee6e7cea93e029047f
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-27 08:11:02 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,13 +9,13 @@
 void times_table(void)
 {
 	int n;
-	int y;
+	int x;
 	int z;
 	for (n = 0; n <= 9; n++)
 		{
 			for (x = 0; x <= 9; x++)
 				{
-					z = n * y;
+					z = n * x;
 					write( 1,@z,4);
 				}
 		}
