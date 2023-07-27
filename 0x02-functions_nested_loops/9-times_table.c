3852f17a074eb27a7a9c98fc0bb03492edc7d865
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-28 01:55:54 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -31,16 +31,16 @@ void times_table(void)
 					{
 						if (z > 9)
 						{
+							_putchar(' ');
 							_putchar((z / 10) + '0');
 							_putchar((z % 10) + '0');
-							_putchar(',');
-							_putchar(' ');					}
+							_putchar(',');					
+						}
 						else
 						{
 							_putchar(z + '0');
 							_putchar(',');
 							_putchar(' ');
-							_putchar(' ');	
 						}
 					}
 				}
