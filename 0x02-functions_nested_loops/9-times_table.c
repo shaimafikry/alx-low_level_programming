780909611a87fb9c119702d20db84ce550cd40eb
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-27 23:56:32 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,31 +6,43 @@
 
 void times_table(void)
 {
-	int n;
-	int x;
-	int z;
+	int n, x, y;
 
 	for (n = 0; n <= 9; n++)
 		{
 			for (x = 0; x <= 9; x++)
 				{
 					z = n * x;
-					if (z > 9)
-					{
-						_putchar((z / 10) + '0');
-						_putchar((z % 10) + '0');
-						_putchar(',');
-						_putchar(' ');					}
-					else
-					{
-						_putchar(z + '0');
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');	
-					}
 					if (x == 9)
 					{
+						if (z > 9)
+						{
+							_putchar((z / 10) + '0');
+							_putchar((z % 10) + '0');
+							_putchar(',');	
+							_putchar('\n');
+						}
+						else
+						{
+						_putchar(z + '0');
 						_putchar('\n');
+						}
+					}
+					else
+					{
+						if (z > 9)
+						{
+							_putchar((z / 10) + '0');
+							_putchar((z % 10) + '0');
+							_putchar(',');
+							_putchar(' ');					}
+						else
+						{
+							_putchar(z + '0');
+							_putchar(',');
+							_putchar(' ');
+							_putchar(' ');	
+						}
 					}
 				}
 		}
