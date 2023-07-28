8804833fe263e830ba7bc6f15dffd550d9299237
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-28 03:04:23 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,1 +1,42 @@
-#!/bin/bash
+#include "main.h"
+/**
+* print_times_table - prints table n numbers
+* Return: no value return
+*/
+void print_times_table(int n)
+{
+	int m, x, z;
+
+		if (n >= 0 || n <= 15)
+		{
+			for (m = 0; m <= n; m++)
+			{
+				for (x = 0; x <= n; x++)
+				{
+					z = m * x;
+					if (x == 0)
+					{
+						_putchar(z + '0');
+					}
+					else
+					{
+						if (z > 9)
+						{
+							_putchar(',');
+							_putchar(' ');
+							_putchar((z / 10) + '0');
+							_putchar((z % 10) + '0');
+						}
+						else
+						{
+							_putchar(',');
+							_putchar(' ');
+							_putchar(' ');
+							_putchar(z + '0');
+						}
+					}
+				}
+				_putchar('\n');
+			}
+		}
+}
