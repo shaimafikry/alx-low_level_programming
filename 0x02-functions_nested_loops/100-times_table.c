96ed0fcc6e0f599e0c2f8dd99251bd09f1945dbc
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-28 03:39:36 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,8 +23,8 @@ void print_times_table(int n)
 					{
 						_putchar(',');
 						_putchar(' ');
-						if (z > 9  && z <= 99)
-						{
+					if (z > 9  && z <= 99)
+					{
 							_putchar(' ');
 							_putchar((z / 10) + '0');
 							_putchar((z % 10) + '0');
