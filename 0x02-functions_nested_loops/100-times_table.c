02c868c2b7c469e24e32afabcf255db0931358ff
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-28 03:29:34 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -34,7 +34,7 @@ void print_times_table(int n)
 							
 							_putchar(',');
 							_putchar(' ');
-							_putchar(((z / 10)  / 10)+ '0');
+							_putchar((z / 100) + '0');
 							_putchar(((z / 10) % 10) + '0');
 							_putchar((z % 10) + '0');
 						}
