30b5b55b8947544a9d22e5458acd2a4f1fbab9bb
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-29 14:46:27 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,20 +11,24 @@ int main(void)
 	{
 		if (x % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
-		else if (x % 5 ==0)
+		else if (x % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (x % 3 == 0 && x % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 
 		}
 		else
 		{
-			printf("%d ", x);
+			printf("%d", x);
+		}
+		if (x != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
