85bd550a31ffcf5ff31cf66ba91589de373fe5b0
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-29 16:16:00 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,44 +6,19 @@
 */
 void print_number(int n)
 {
-	if (n >= 0)
+	int p;
+
+	if (n < 0)
 	{
-		if (n > 999)
-		{
-			_putchar((n / 1000) + '0');
-			_putchar((n / 100 % 10) + '0');
-			_putchar((n / 10 % 10) + '0');
-		}
-		else if (n > 99)
-		{
-			_putchar((n / 100) + '0');
-			_putchar((n / 10 % 10) + '0');
-		}
-		else if (n > 9)
-		{
-			_putchar((n / 10) + '0');
-		}
-		_putchar((n % 10) + '0');
-	}
-	else
-	{
-		n = n * -1;
+		p = n * -1;
 		_putchar('-');
-		if (n > 999)
-		{
-			_putchar((n / 1000) + '0');
-			_putchar((n / 100 % 10) + '0');
-			_putchar((n / 10 % 10) + '0');
-		}
-		else if (n > 99)
-		{
-			_putchar((n / 100) + '0');
-			_putchar((n / 10 % 10) + '0');
-		}
-		else if (n > 9)
-		{
-			_putchar((n / 10) + '0');
-		}
-		_putchar((n % 10) + '0');
 	}
+
+	p = n / 10;
+	if (p != 0)
+	{
+		print_number(p);
+	}
+		
+		_putchar((p % 10) + '0');
 }
