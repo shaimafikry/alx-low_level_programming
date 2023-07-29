781f9e52aaace6926f8dd0cb5f2a7e4b7431f71d
shimaa <shaimafikry@yahoo.com>
2023-07-29 01:17:36 -0700

    editing files

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+/**
+* more_numbers - print 0-14 numbers 0 times
+* Return: no return
+*/
+void more_numbers(void)
+{
+	char i;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (i = 0; i <= 14'; i++)
+		{
+			if (i > 9)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			else
+			{
+				_putchar(i + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
