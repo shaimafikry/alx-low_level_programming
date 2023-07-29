a895fe1da1f348281525218d70e9da0f42cf8d03
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-07-29 10:29:13 +0300

    Update 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _isupper - prins upper case letter
+* @c : integr
+* Return: 1 if uppercase
+*	0 otherwise
+*/
+int _isupper(int c)
+{
+	if(c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
