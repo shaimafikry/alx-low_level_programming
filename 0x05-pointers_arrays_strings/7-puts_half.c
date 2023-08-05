f878448a16ea5c79118517bb2105455099055bbf
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-05 11:57:40 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,12 +11,13 @@ void puts_half(char *str)
 	int i;
 	int count = 0;
 	int n;
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		count++;
 	}
 	n = (count - 1) / 2;
-	for (i = n - 1; str[i] != '\0'; i++)
+	for (i = n + 1; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
