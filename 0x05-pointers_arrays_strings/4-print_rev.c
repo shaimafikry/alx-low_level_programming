230151a5f774127f1df5fabee9ae790f98686d3f
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-05 11:45:28 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,13 @@ void print_rev(char *s)
 	int i;
 	int count;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		count++;
 	}
 	for (i = count - 1 ; i == 0; i--)
 	{
-		_putchar(str[i]);
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
