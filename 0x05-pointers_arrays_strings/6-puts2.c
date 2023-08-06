c5a4ad6e9cc5a0eee7ad68e117e0b2d1ee00a5a9
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-06 13:50:40 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,8 +9,13 @@
 void puts2(char *str)
 {
 	int i = 0;
+ int count = 0;
 
-	for (i = 0;str[i]!='\0';i+2)
+for (i = 0; str[i] != '\0; i++)
+	{
+		count++;
+	}
+	for (i = 0; i < count; i+2)
 	{
 		_putchar(str[i]);
 	}
