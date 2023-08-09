5f924eec48a8fe2ea5f79f3ccb2d6c5958349ab3
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-10 01:05:18 +0300

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,6 +11,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	if (n == 1)
+	{
+		printf("%d", a[i]);
+	}
+	else
+	{
 	for (i = 0; i != n; i++)
 	{
 		if (i < n - 1)
@@ -21,6 +27,7 @@ void print_array(int *a, int n)
 		{
 		printf("%d", a[i]);
 		}
+	}
 	}
 	putchar('\n');
 }
