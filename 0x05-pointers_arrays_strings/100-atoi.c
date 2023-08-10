867c77a24476984506908f558ff5524fc57733aa
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-10 11:25:00 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,23 +11,32 @@ int _atoi(char *s)
 	int i, neg = 0;
 	int result = 0;
 	int num = 0;
+	int check = 0;
 
 	for(i = 0; s[i] != '\0'; i++)
 	{
-		if(s[i] == '-')
-			neg++;
-		else if(s[i] <= '0' && s[i] > '9')
+		if(s[i] >= '0' && s[i] <= '9')
 		{
-			result = 0;
-		}
-		else(s[i] > '0' && s[i] <= '9')
-		{
-			num = (s[i] - '0');
-			result *= 10 + num;
+			check++;
 		}
 	}
-	if(neg / 2)
-		return (result);
+	if (check == 0)
+		return (0);
 	else
-		return (-result)
+	{
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			if (s[i] == '-')
+				neg++; 
+			else if (s[i] >= '0' && s[i] <= '9')
+		{
+				num = (s[i] - '0');
+				result *= 10 + num;
+		}
+		}
+			if(neg % 2 == 0)
+				return (result);
+			else
+				return (-result)
+	}
 }
