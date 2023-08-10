d550b50ac3ed068925609f13be6366de7b555b5e
shimaa <shaimafikry@yahoo.com>
2023-08-10 09:14:23 -0700

    adding files

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenates two strings
+ * @s1 : a pointer to string 1
+ * @s2 : a pointer to string 2
+ * @n : number of bytes to concat
+ * Return: return a new pointer to s1
+ */
