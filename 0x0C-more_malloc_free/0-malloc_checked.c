22bb764eb94ad6eeae63d381be7bfa6de46122d9
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-11 15:12:24 +0300

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 /**
  * malloc_checked - assigns memory to a pointer
- * @b : unsigned integer
- * Return : pointer to a memory
+ * @b: unsigned integer
+ * Return: pointer to a memory
  */
 void *malloc_checked(unsigned int b)
 {
