487d5a1ee226d05f83907e1277ce4cacf245f96e
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-11 15:03:10 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,9 +11,13 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	if (new_size == old_size)
+	{
 		return (ptr);
+	}
 	if (new_size == 0 && ptr != NULL)
+	{
 		return (NULL);
+	}
 	if (new_size > old_size)
 	{
 		if (ptr == NULL)
