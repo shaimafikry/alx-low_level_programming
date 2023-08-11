d6186dea81e2c74f392db1a800e4acc0b8d7d049
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-11 13:27:50 +0300

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,9 +1,23 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * string_nconcat - concatenates two strings
- * @s1 : a pointer to string 1
- * @s2 : a pointer to string 2
- * @n : number of bytes to concat
- * Return: return a new pointer to s1
+ * array_range - creats new array
+ * @min : a pointer to string 1
+ * @max : a pointer to string 2
+ * Return: return a new pointer to newly created array
  */
+int *array_range(int min, int max)
+{
+	int *new_arr;
+	int i ;
+
+	new_arr = malloc(max);
+	if (new_arr == NULL || min > max)
+		return (NULL);
+	for (i = 0; min <= max; i++)
+	{
+		new_arr[i] = min;
+		min++;
+	}
+	return (new_arr);
+}
