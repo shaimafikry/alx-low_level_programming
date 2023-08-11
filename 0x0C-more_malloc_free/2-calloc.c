4aefedae8b629da5d5c0f6aea69c96b36662736c
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-11 11:06:01 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,20 +1,21 @@
 #include "main.h"
-#include <stdlib.h>
-#inclue <string.h>
+#include < stdlib.h >
+#include < string.h >
 /**
  * _calloc - allocates memory for an array
  * @nmemb : a pointer to string 1
  * @size : a pointer to string 2
  * Return: returns a pointer to the allocated memory.
- *         If nmemb or size is 0, then _calloc returns NULL
- *         If malloc fails, then _calloc returns NULL 
+ * 	If nmemb or size is 0, then _calloc returns NULL
+ * 	If malloc fails, then _calloc returns NULL 
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *mem;
+
 	mem = malloc(nmem * size);
-	if ( (size == 0 || nmemb == 0)|| mem == Null)
+	if ((size == 0 || nmemb == 0) || mem == Null)
 		return (Null);
-	else 
+	else
 		return (mem);
 }
