1cd17fbef5b76a881ba75c2b1f17de3365d7befc
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-13 13:43:31 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	else if (mem == NULL)
 		return (NULL);
-	while (num < nmemb * size)
+	while (num < nmemb)
 	{
 		mem[num] = 0;
 		num++;
