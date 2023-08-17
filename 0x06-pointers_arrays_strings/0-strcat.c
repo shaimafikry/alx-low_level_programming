5212a583a0682ca3382e68e5044b6f0653e8544b
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-18 00:31:33 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,11 +2,26 @@
 #include "main.h"
 
 /**
- * _strcpy - copy strings src to dest
+ * _strcat - cat strings src to dest
  * @dest : a pointer to char
  * @src  : a pointer to char
  * Return: return pointer to dest
  */
 
 *_strcat(char *dest, char *src)
+{
+int i = 0;
+int count = 0;
 
+while (dest[i] != '\0')
+{
+count++;
+i++;
+}
+i = 0;
+while(src[i] != '\0')
+{
+dest[count + 1] = src[i];
+}
+return (dest);
+}
