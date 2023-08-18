8a7d362c42edbddfdcb639d9de347a914b08d891
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-18 09:18:38 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,35 @@
 #include "main.h"
 
 /**
- * _strcpy - copy strings src to dest
+ * _strncat - adding n src to dest
  * @dest : a pointer to char
  * @src  : a pointer to char
  * Return: return pointer to dest
  */
 
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+int count, count2;
+
+count = 0;
+count2 = 0;
+i = 0;
+while (dest[i] != '\0')
+{
+count++;
+}
+while (src[i] != '\0')
+{
+count2++;
+}
+if (n > 0)
+{
+while(i < n)
+{
+dest[count +1] = src[i];
+i++;
+}
+}
+return (dest);
+}
