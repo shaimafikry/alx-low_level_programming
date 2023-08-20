dd66ffbab95df586fe23d89729305c54a2b8d9af
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-20 09:24:03 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: array
  * Return: 0 always success
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc);
 	return (0);
