9580aabecfac9cf9a176933c90ce627ecbede471
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-20 09:34:04 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,9 +1,25 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 /**
- * rev_string - prints string into stdout reversed
- * @s : a pointer to char
- * Return: no return
+ * main - entry point
+ * @argc: number of argument
+ * @argv: array
+ * Return: 0 always success
  */
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul;
 
+	if (argc < 2)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
+	{
+		mul = argv[0] * argv[1];
+		printf("%d\n", mul);
+	}
+	return (0);
+}
