f463544b1414358f67200d284f6e5ee441f65f58
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-20 09:44:44 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,6 +11,8 @@ int main(int argc, char *argv[])
 	int mul;
 	int n1,n2;
 
+	n1 = argv[0];
+	n2 = argv[1];
 	if (argc < 2)
 	{
 		printf("Error");
@@ -18,8 +20,8 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		n1 = (argv[0] - '0');
-		n2 = (argv[1] - '0');
+		n1 = (n1 - '0');
+		n2 = (n2 - '0');
 		mul = n1 * n2;
 		printf("%d\n", mul);
 	}
