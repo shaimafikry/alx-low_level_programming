ec11f20b15a4dcfed0ea40526757d5643ed38e8d
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-20 10:32:14 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (argv[i] < '0' && argv[i] > '9')
+			if (argv[i] < 48 && argv[i] > 57)
 			{
 				printf("Error");
 				printf("\n");
