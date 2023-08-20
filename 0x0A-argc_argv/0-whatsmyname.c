e1dfe4b2a75eaedfbf5d36397fe06fa8d459be40
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-20 09:06:56 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,24 +1,20 @@
- #include "main.h"
+#include "main.h"
 /**
-* @format : pointer to format
-* @arg : 
-* Return : count
+* main - entry point
+* @argc : number of argument 
+* @argv : array
+* Return : 0 always success
 * /
-* 
-int main (argc ,*argv)
+int main (int argc, char *argv[])
 {
- int i;
-for ( i = 0; i < argc; i++)
-{
-for (m =0; argv[i][m] != '\0'; m++)
-  _putchar(argv[i][m]);
-}
+	int i;
+	int m;
 
+	for ( i = 0; i < argc; i++)
+	{
+		for (m =0; argv[i][m] != '\0'; m++)
 
-
-
+			_putchar(argv[i][m]);
+	}
 return (0);
-
-
 }
-
