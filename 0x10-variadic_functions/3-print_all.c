92c4b6b92c14d768c60e322b21d2588c1a255b8c
shaimaa <shaimafikry@yahoo.com>
2023-08-23 15:04:27 -0700

    edit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+/**
+* sum_them_all - sum all numbers
+* @n: unsigned integer
+* Return: sum
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	usigned int sum;
+	int i;
+
+	sum = 0;
+	va_list num;
+	va_start(num , n);
+	if (n == 0)
+		return (0):
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(num, int);
+	}
+	va_end(num);
+	return (sum);
+}
