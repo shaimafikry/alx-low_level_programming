fb915c21ce483217d34fbf92a5465982873139f1
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-24 00:58:18 +0300

    Update 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* sum_them_all - sum all numbers
+* @n: unsigned integer
+* Return: sum;
+
+int sum_them_all(const unsigned int n, ...)
+{
+	usigned int sum;
+	int i;
+
+	sum = 0;
+	va_list num;
+	va_start(n,num);
+	if (n == 0)
+		return (0):
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(num,int);
+	}
+	va_end(num);
+	return (sum);
+}
