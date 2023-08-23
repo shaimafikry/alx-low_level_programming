23a143096f95d1ae0685b5d94d6ed0b75deb142c
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-24 01:44:28 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include <stdarg.h>
 /**
- * Fnction decleration
- *
+ * Function decleration
+ * 
  *
  */
-char _putchar(char c);
+void _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
