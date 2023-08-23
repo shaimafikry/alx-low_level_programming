c1d1c7eccf6608b510f4904ffe1bee8c05066120
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-24 01:17:49 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#include "main.h"
 #include <stdlib.h>
 #include <stdarg.h>
 /**
@@ -13,5 +11,3 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-#endif
