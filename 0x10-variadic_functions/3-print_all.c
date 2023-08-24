14c4e4bab31fc7b3734dce3ed9a13fc11fb32998
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-24 06:44:50 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,18 +8,19 @@
 */
 void print_all(const char * const format, ...)
 {
-	unsigned int i;
+	int i;
 	va_list str;
 
-	va_start(str, n);
-	for (i = 0; i < n; i++)
+	va_start(str, format);
+	while (va_list str)
 	{
 		if (va_arg(str, char*) == NULL)
 			printf("nil");
 		printf("%s",va_arg(str, char*);
-		if (separator != NULL)
-			printf("%s",seprator);
+		printf("%d",va_arg(str, int));
+		printf("%c",va_arg(str, char));
+		printf("%f",va_arg(str, float));
 	}
-	va_end(num);
+	va_end(str);
 	_putchar('\n'):
 }
