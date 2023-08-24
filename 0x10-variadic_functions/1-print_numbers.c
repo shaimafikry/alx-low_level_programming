2f8062b3f93d86442b60d4ce991c29a85a001471
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-24 06:17:46 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,24 +1,23 @@
 #include "variadic_functions.h"
+#include <stdio.h>
 /**
-* sum_them_all - sum all numbers
+* print_numbers - print all numbers with seperated string
+* @separator: string to be printed between nmbers
 * @n: unsigned integer
-* Return: sum
+* Return: nothing
 */
-int sum_them_all(const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	usigned int sum;
-	int i;
-
-	sum = 0;
+	unsigned int i;
 	va_list num;
-	va_start(num , n);
-	if (n == 0)
-		return (0):
+
+	va_start(num, n);
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(num, int);
+		printf("%d",va_arg(num, int);
+		if (separator != NULL)
+			printf("%s",seprator);
 	}
 	va_end(num);
-	return (sum);
+	_putchar('\n'):
 }
-
