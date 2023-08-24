6aaecc31bed91b9e3ba58e211557d3266e537852
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-24 09:08:25 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(num, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d",va_arg(num, int);
+		printf("%d", va_arg(num, int));
 		if (separator != NULL)
 			printf("%s",seprator);
 	}
