d4c254a11d0983706c815cda944ab87043532982
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-25 07:30:29 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,1 +1,15 @@
 #include "main.h"
+/** 
+  * _print_rev_recursion - prints a string inreverse
+  * @s: string
+  * Return: no return
+  */
+void _print_rev_recursion(char *s)
+{
+	if (*s != 0)
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+}
