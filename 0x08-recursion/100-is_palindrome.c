496524f5194f72da0174ce47eef4e93657c16a0c
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-26 01:22:23 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,11 +6,37 @@
   */
 int is_palindrome(char *s)
 {
-	if (*s != 0)
-    {
-        if (_puts_recursion(s + 1) == _print_rev_recursion(s + 1))
+	if (*s == 0)
 		return (1);
 	else
-		return (0);
-    }
+		return (check_str(*s));
+}
+/**
+  * check_str - cheks a if a string is palindrom
+  * @s: string
+  * Return:  return 1 true 0 false
+  */
+
+int check_str( char *s)
+{
+int i, m;
+
+m = 0
+i = 0;
+while (s[i] != '\0')
+{
+	i++;
+}
+i = i - 1;
+while (i >= 0)
+{
+	if (s[i] == *s)
+	{
+		i--;
+		m++;
+	}
+	else
+		return 0;
+}
+	return 1;
 }
