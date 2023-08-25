d9548d9e803ad0ec9aea16ed0c782c5131d9c9e9
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-25 20:00:43 +0300

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,1 +1,12 @@
 #include "main.h"
+/**
+  * factorial - calculate a string length
+  * @n: string
+  * Return: factorial of a number
+  */
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
