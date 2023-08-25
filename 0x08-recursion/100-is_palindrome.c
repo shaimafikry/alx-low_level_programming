e0767248da6652cf8a6bbcd346c1764ced8f8de3
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-26 00:46:07 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,1 +1,16 @@
 #include "main.h"
+/**
+  * _print_rev_recursion - prints a if a string is palindrom
+  * @s: string
+  * Return:  return 1 true 0 false
+  */
+int is_palindrome(char *s)
+{
+	if (*s != 0)
+    {
+        if (_puts_recursion(s + 1) == _print_rev_recursion(s + 1))
+		return (1);
+	else
+		return (0);
+    }
+}
