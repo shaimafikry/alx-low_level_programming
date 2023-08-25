405bb621c7d7338b95f686a34f48ee751e008831
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-25 23:57:14 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,10 +8,10 @@ int is_prime_number(int n)
 {
 	int x;
 
-	if ( n <= 0 || n == 1)
+	if (n <= 0 || n == 1)
 		return (0);
 	x = n - 1;
-	if (n == 2)
+	if (n == 2 || n == 3)
 		return (1);
 	if (is_prime_number(n) % x == 0)
 		return (is_prime_number(x));
