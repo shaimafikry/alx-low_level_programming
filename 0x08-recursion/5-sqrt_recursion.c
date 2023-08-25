6a42cd554aee8b2a151d4a40f1971af64cf91243
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-25 23:14:54 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,7 +8,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0 || n % 2 != 0 )
 		return (-1);
-	if (n == 1)
+	if (n == 0)
 		return (1);
-	return (n/ _pow_recursion(n - 2));
+	return (n / _sqrt_recursion(n - 2));
 }
