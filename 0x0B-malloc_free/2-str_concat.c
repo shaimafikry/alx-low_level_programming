f849ffaf89992e32caf9666081ef9a553975de6a
shaimaa <shaimafikry@yahoo.com>
2023-08-25 21:00:18 -0700

    edit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,5 @@
+#include "main.h"
+/**
+  * _print_rev_recursion - prints a if a string is palindrom
+  * @s: string
+  * Return:  return 1 true 0 false
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+For this project, we expect you to look at this concept:
+
+Automatic and dynamic allocation, malloc and free
