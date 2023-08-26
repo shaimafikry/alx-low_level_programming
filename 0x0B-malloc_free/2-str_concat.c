99af8b5b5b089090bac381e9688a4e5b767fcf67
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-26 20:35:06 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ char *str_concat(char *s1, char *s2)
 	count = 0;
 	m = 0;
 	if (s1 == NULL)
-		s2 = "";
-	if (s2 == NULL)
 		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	if (s1 == NULL && s2 == NULL)
 	{
 		return ("");
