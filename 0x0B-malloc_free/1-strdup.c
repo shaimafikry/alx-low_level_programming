f3bf007d32d269f3564e0482c9a6447d43ec3054
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-26 18:33:13 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,10 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-  * create_array - prints a if a string is palindrom
-  * @c: character
-  * @size: araay size
-  * Return:  a pointer to the array, or NULL if it fails
+  * _strdup - prints a copy string 
+  * @s: pointer to dtring
+  * Return:  a pointer to the copied string , or NULL if it fails
  */
 char *_strdup(char *str)
 {
@@ -15,20 +14,20 @@ char *_strdup(char *str)
 	count = 0;
 	if (str == NULL)
 		return (NULL);
-	for (i =0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		count++;
 	}
 		
-	ptr = malloc(sizeof(char) * count);
+	ptr = malloc(sizeof(char) * count + 1);
 	i = 0;
 	if (ptr != NULL)
 	{
 		while (i < count)
-	{
-		ptr[i] = str[i];
-		i++;
-	}
+		{
+			ptr[i] = str[i];
+			i++;
+		}
 	return (ptr);
 	}
 	else
