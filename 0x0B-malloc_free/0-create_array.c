fdfd950441ba4b97aeeca886522adbbe5dfefabf
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-26 11:58:12 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,7 +9,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
-	int i;
+	/*int i;*/
 
 	if (size == 0)
 		return (NULL);
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	{
 	while (*arr !=0 )
 	{
-		arr = c;
+		*arr = c;
 		arr++;
 	}
 	return (arr);
