73e16732c41345b3784beb933c9fbecfbadf13f2
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-26 19:45:22 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,28 +9,39 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *arr;
-	int len1, len2, i;
+	int count, i, m;
 
-	len1 = 0;
-	len2 = 0;
+	count = 0;
+	m = 0;
 	if (s1 == NULL)
 		s2 = "";
 	if (s2 == NULL)
 		s1 = "";
 	if (s1 == NULL && s2 == NULL)
 	{
-		return ("");
+		return "";
 	}
 	for ( i = 0; s1[i] != '\0'; i++)
-		len1++;
+		count++;
 	for ( i = 0; s2[i] != '\0'; i++)
-		len2++;
-	arr = malloc(sizeof(char) * (len1 + len2 + 1));
+		count++;
+	count++;
+	arr = malloc(sizeof(char) * count);
 	if (arr == NULL)
 		return (NULL);
-	while (i < len1)
-		arr[i] = s1[i];
-	while ( i < len2)
-		arr[i] = s2[i];
-	return (arr);
+	else
+	{
+		for (i = 0; i < count; i++)
+		{
+			if (s1[i] == '\0')
+				while ( i < count)
+				{
+					arr[i] = s2[m];
+					i++;
+					m++;
+				}
+			arr[i] = s1[i];
+		}
+		return (arr);
+	}
 }
