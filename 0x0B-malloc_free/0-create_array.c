046d487a67f091b29cc1d7c71e4d7c95b32ea96c
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-26 07:13:11 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 /**
-  * _print_rev_recursion - prints a if a string is palindrom
-  * @s: string
-  * Return:  return 1 true 0 false
+  * create_array - prints a if a string is palindrom
+  * @c: character
+  * @size: araay size
+  * Return:  a pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i;
+
+	if (size == 0)
+		return (NULL);
+	arr = malloc(sizeof(char) * size);
+	if ( arr != NULL)
+	{
+	for (i = 0; arr[size] != '\0'; i++)
+		arr[i] = c;
+	return (arr);
+	}
+	else
+		return (NULL);
+}
