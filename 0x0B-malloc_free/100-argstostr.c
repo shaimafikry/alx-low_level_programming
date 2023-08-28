cd1be491bbcebc4c69c015d403042640521b63c9
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-28 09:33:25 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -15,7 +15,7 @@ char *argstostr(int ac, char **av)
 	if ( ac == 0 || av == NULL)
 		return (NULL);
 
- 	str = malloc(sizeof(char) * (count + (ac - 2));
+ 	str = malloc(sizeof(char) * (count + (ac - 2)));
 	if (str == NULL)
 	{
 		free (str);
