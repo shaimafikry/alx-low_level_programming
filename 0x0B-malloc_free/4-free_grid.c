edf0d2afc77b6642f46eea7ee603a54ed37c2d73
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-28 09:23:47 +0300

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,17 +1,18 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-* free_grid
+* free_grid - free previous alocated memory
 * @grid: pointer to be freed
-* @height pointer length
+* @height: pointer length
 * Return: no return
 */
 void free_grid(int **grid, int height)
 {
 	int i;
 
-	for (i = 0; i < height; i++)
-		{
-			free(grid[i]);
-		}
+	while (**grid != 0)
+	{
+		free(*grid);
+		*grid++;
+	}
 }
