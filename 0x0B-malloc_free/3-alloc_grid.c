3e26f9a94f0a6b184b460203f218806adfda7b3b
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-28 09:06:25 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	ptr = malloc((height) * sizeof(int *));
+	ptr = malloc((height) * sizeof(int));
 	if (ptr == NULL)
 	{
 		free(ptr);
