0f0a4f528799d7606fe275afcd7b160ea83456c4
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-28 10:54:03 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,45 @@
 #include "main.h"
 /**
-  * _print_rev_recursion - prints a if a string is palindrom
-  * @s: string
-  * Return:  return 1 true 0 false
+  * _strtow - split two stringds
+  * @str: string
+  * Return:  return pointer to string with words
+char **strtow(char *str)
+{
+	int i, j;
+	char **str_s;
+	int len;
+
+	if (str == NULL || str == "")
+		return (NULL);
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+	len++;
+	str_s = malloc (sizeof(char *) * len);
+	if (str_s == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+	{
+		str_s[i] = malloc(sizeof(char) * len);
+		if (str_s[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(str_s[j]);
+			free(str_s);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < len; i++)
+	{
+		for (j = 0; j < len; j++)
+		{
+			if (str[j] != ' ')
+				str_s[i][j] = str[j];
+		}
+	}
+	strs_s[len] = NULL;
+	return (str_s);
+				
+}
