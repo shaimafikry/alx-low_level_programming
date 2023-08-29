ae5de71f09b1f1b2e3389ec50a5ef31b476086e1
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-29 22:38:12 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,1 +1,15 @@
-#include
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - structure for collecting info of  a dog
+* @name: char
+* @age: flaot
+* @owner: char
+*/
+struct dog {
+char *naame;
+float age;
+char *owner;
+}
+
+#endif
