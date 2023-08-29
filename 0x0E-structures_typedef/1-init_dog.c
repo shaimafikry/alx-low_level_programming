4e7cbeb2315e30727bbb8a4fd7f5310c84d0b45c
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-29 23:00:58 +0300

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include dog.h;
+#include "dog.h"
 /**
 * init_dog - func to iniate struct
 * @struct dog d: child of a struct
@@ -10,7 +10,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	struct dog *d;
 	d = malloc(sizeof(struct dog);
-	d.name = name;
-	d.age = age;
-	d.owner = owner;
+	if (d == NULL)
+		return (NULL);
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
 }
