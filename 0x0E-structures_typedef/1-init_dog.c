d6838ef9dc7681f92e5c2932c77f8a57b00f1d73
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-30 02:10:26 +0300

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,8 +9,36 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	int i, len1, len2;
+
+	len1 = 0;
+	len2 = 0;
+	i = 0;
 	d = malloc(sizeof(struct dog));
-	d->name = name;
+	while ( name[i] != '\0')
+	{
+		len1++;
+		i++;
+	}
+	d->name = malloc(len1 + 1);
+	while ( owner[i] != '\0')
+	{
+		len2++;
+		i++;
+	}
+	d->owner = malloc(len2 + 1);
+	while ( name[i] != '\0')
+	{
+		d->name[i] = name[i];
+		i++;
+	}
+	d->name[len] = '\0';
 	d->age = age;
-	d->owner = owner;
+	i = 0;
+	while ( owner[i] != '\0')
+	{
+		d->owner[i] = name[i];
+		i++;
+	}
+	d->owner[len2] = '\0';
 }
