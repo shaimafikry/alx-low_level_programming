4802bfce9a4ba73cf8608bfda96ea9078de16cdf
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-29 22:51:33 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,13 +13,6 @@ float age;
 char *owner;
 };
 
-struct dog 
-{
-char *name;
-float age;
-char *owner;
-};dog_t
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
