0e5b3fe9be746c25556a7085032158fd0b819081
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-30 02:28:18 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,1 +1,24 @@
-add
+#include "dog.h"
+#include <stdlib.h>
+/**
+* init_dog - func to iniate struct
+* @d: child of a struct
+*/
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name)
+			printf("Name: %s",d->name);
+		else
+			printf("(nil)");
+		if (d->age)
+			printf("Age: %f",d->age);
+		else
+			printf("(nil)");
+		if (d->owner)
+			printf("Owner: %s",d->owner);
+		else
+			printf("(nil)");
+	}
+}
