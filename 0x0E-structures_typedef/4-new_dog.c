833ec4ea5c38a738107203a30c65816c13c8d8ab
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-30 02:45:31 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,7 +8,7 @@
 * @owner: string
 * Return: pointer to a new struct
 */
-dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 	if (d != NULL)
