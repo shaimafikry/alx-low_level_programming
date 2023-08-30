0030627b33329e01b57b84fe778ba6a61f0a4e49
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-30 22:01:09 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stddef.h>
+#include <stdio.h>
 
 /**
  * print_list - prints all the elements of a list_t list
@@ -12,15 +13,15 @@ size_t print_list(const list_t *h)
 	int count,i ;
 
 	count = 0;
-	while  ( (*h).next != NULL)
+	while  ( (*h).*next != NULL)
 	{
-		next =  (*h).next;
+		*next =  (*h).*next;
 		count++;
 	}
 	
 	if ((*h).str == NULL || (*h).len == 0)
 		printf("[0] (nil)");
-	if (i = 0; i < count; i ++)
+	if (i = 0; i < count; i++)
 		printf("%s", (*h).str);
 
 	
