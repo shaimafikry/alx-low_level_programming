8aa835ab09a32c7aa216c241c884ba53d9437da3
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-30 07:32:20 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,10 +19,12 @@ int (*get_op_func(char *s))(int, int)
 	};
     int i;
 
+	i = 0;
 	while (ops[i].op != NULL)
 	{
 		if (s[1] == ops[i].op[1])
 			return (ops[1].f);
+		i++;
 	}
 	return (NULL);
 }
