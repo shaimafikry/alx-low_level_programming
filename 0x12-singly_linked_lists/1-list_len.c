ffad0f0ac13c185638c5d6540ec155ac5006994a
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-30 22:11:37 +0300

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,19 @@
-1-list_len.c
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to list
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *curr = h;
+	while(curr != NULL)
+	{
+		count++;
+		curr = curr->next;
+	}
+	return count; //return the total count of nodes in the list
+}
