f2d5d5c0fcd14a8d6fff29e8761ee638c58e0af8
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-30 04:47:12 +0300

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
- * array_iterator - that executes a function given as a parameter on each element of an array
+ * array_iterator - that executes a function given as a parameter
  * @array: array
  * @size: size of arry
  * @action: function
@@ -8,10 +8,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i;
+	unsigned long int i;
 
 	i = 0;
-	if (array != NULL)
+	if (array != NULL || size == 0)
 	{
 		while (i != size)
 		{
