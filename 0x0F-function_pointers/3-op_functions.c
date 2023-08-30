856357c65f13d0bd22e2deac8bee2e21e9648c5a
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-30 05:53:24 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,1 +1,31 @@
-3-op_functions.c
+#include "function_pointers.h"
+/**
+ * op_add -  add two digits
+ * Return:  return result
+ */
+int op_add(int a, int b)
+	return (a + b);
+/**
+ * op_sub -  sub two digits
+ * Return: resul
+ */
+int op_sub(int a, int b)
+	return (a - b;
+/**
+ * op_mul -  mull two digits
+ * Return: return result
+ */
+int op_mul(int a, int b)
+	return (a * b);
+/**
+ * op_div -  div two digits
+ * Return:  result
+ */
+int op_div(int a, int b)
+	return (a /b);
+/**
+ * op_mod -  a mod b
+ * Return: result
+ */
+int op_mod(int a, int b)
+	return (a % b);
