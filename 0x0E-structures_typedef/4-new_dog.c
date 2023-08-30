65217c853b575bdbbc25d3b26dc02d6eddce6552
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-30 03:11:40 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,47 +13,32 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d;
 	int i, len1, len2;
 
-	len1 = 0;
-	len2 = 0;
-	i = 0;
-	while ( name[i] != '\0')
-	{
-		len1++;
-		i++;
-	}
-	i = 0;
-	while ( owner[i] != '\0')
-	{
-		len2++;
-		i++;
-	}
+	len1 = 0, len2 = 0, i = 0;
 	d = malloc(sizeof(dog_t));
-	if (d != NULL)
-	{
-		d->name = malloc(len1 + 1);
-		d->owner = malloc(len2 + 1);
-		i = 0;
-		while (name[i] != '\0')
-		{
-			d->name[i] = name[i];
-			i++;
-		}
-		d->name[len1] = '\0';
-		d->age = age;
-		i = 0;
-		while (owner[i] != '\0')
-		{
-			d->owner[i] = owner[i];
-			i++;
-		}
-		d->owner[len2] = '\0';
-	}
-	else
+	if (d != NULL )
+		return (NULL);
+	while ( name[i++] != '\0')
+		len1++;
+	i = 0;
+	while ( owner[i++] != '\0')
+		len2++;
+	d->name = malloc(len1 + 1);
+	d->owner = malloc(len2 + 1);
+	if (d->name == NULL || d->owner == NULL)
 	{
 		free(d->name);
 		free(d->owner);
 		free(d);
 		return (NULL);
 	}
+		i = 0;
+		while (name[i++] != '\0')
+			d->name[i] = name[i];
+		d->name[len1] = '\0';
+		d->age = age;
+		i = 0;
+		while (owner[i++] != '\0')
+			d->owner[i] = owner[i];
+		d->owner[len2] = '\0';
 	return (d);
 }
