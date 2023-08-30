e1e4c40ab444ec8f88b800eda36dc84569882a75
shaimaa <shaimafikry@yahoo.com>
2023-08-29 18:23:15 -0700

    edit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name -  function that prints a name
+ * @name: string
+ * @f: pointer to a func
+ * 
+ * Return: no return
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
+	
