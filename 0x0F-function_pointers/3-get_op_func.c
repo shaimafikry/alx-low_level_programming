0f0879fdb910a95f2f116a5fe6179f5683cd16e7
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-30 05:47:19 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,1 +1,27 @@
-3-get_op_func.c
+#include "function_pointers.h"
+/**
+ * get_op_func -  function that select which opr to deal with
+ * @s: string
+ * 
+ * Return: null when no op found 
+ * Return: no return
+ */
+int (*get_op_func(char *s))(int, int);
+{
+	op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
+
+	while (ops[i] != NULL)
+	{
+		if (s[1] == ops[i].op[1])
+			return ops[1].f();
+	}
+	return (NULL);
+}
