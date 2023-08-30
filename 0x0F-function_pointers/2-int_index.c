abc493dca18f5c640aa73cff0af3aa2a68b8ab76
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-30 05:21:31 +0300

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,1 +1,22 @@
-2-int_index.c
+#include "function_pointers.h"
+/**
+ * int_index - that executes a function given as a parameter
+ * @array: array of integers
+ * @size: size of arry
+ * @cmp: function
+ * Return: return -1 when fails or no integer found
+ *	return integer found when sucess
+ */
+int int_index(int *array, int size, int (*cmp)(int));
+{
+	int i, n;
+
+	if (size <= 0)
+		return (-1);
+	while (i < size) 
+	{
+		if (cmp(array[i]);)
+			return (i);
+	}
+	return (0);
+}
