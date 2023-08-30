2db43b274e9975ba1bc35ade82cd532c5eb10845
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-08-30 05:22:43 +0300

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,7 +9,7 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, n;
+	int i = 0;
 
 	if (size <= 0)
 		return (-1);
@@ -17,6 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		if (cmp(array[i]);)
 			return (i);
+		i++;
 	}
 	return (0);
 }
