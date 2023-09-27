74a0e6668314b88923a7b7ae6f031c6d94334780
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-09-27 07:12:30 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,15 +7,14 @@
  * @n: numbers to be filled
  * Return: pointer to the area s
  */
- 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for(i = 0;i != n; i++)
+	for (i = 0; i != n; i++)
 	{
 		s[i] = b;
 	}
-	return s;
+	return (s);
 }
 
