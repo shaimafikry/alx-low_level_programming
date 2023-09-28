7feac09a7d02203655e5d26d2cf44d616f9f635d
shaimaa <shaimafikry@yahoo.com>
2023-09-27 23:52:16 -0700

    add files

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strstr - Write a function that locates a substring.
+ * @haystack: string
+ * @needle: dubdtring
+ * Return: pointer to the begining of substring
+ * 	null if not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+        int m, i, size;
+
+        i = 0;
+        size = 0;
+        m= 0;
+        for (m = 0; needle[m] != '\0'; m++)
+                size++;
+        while (haystack[i] != '\0')
+        {
+            if (haystack[i] == needle[m])
+            {
+                m++;
+            }
+                if (size == m)
+                        return (haystack+i);
+                i++;
+        }
+        return (NULL);
+}
