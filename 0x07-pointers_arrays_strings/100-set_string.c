ba8be0be5ab8102d8de2bdfc4cca2253eaa8a771
shaimaa <shaimafikry@yahoo.com>
2023-09-29 12:52:53 -0700

    add fils

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string -  function that sets the value of a pointer to a char.
+ * @s: 2di ptr
+ * @to: destination
+ * Return: no return
+ */
+void set_string(char **s, char *to)
+{
+
+}
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,10 +8,12 @@ void print_chessboard(char (*a)[8])
 {
 	int i, m;
 
-	i = 0;
-	m = 0;
-	for (i = 0, m = 0; a[i][m] != '\0'; i++, m++)
+	for (i = 0; a[i] != '\0'; i++)
 	{
-		_putchar(a[i][m]);
+		for ( m = 0; a[i][m] != '\0'; m++)
+		{
+			_putchar(a[i][m]);
+		}
+		_putchar('\n');
 	}
 }
