9e7e9464d9c5f38acf28cd8cf0b615e860eaf704
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-09-29 14:36:11 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,12 +12,14 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] <= '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (s);
 		}
 	}
+	if (c == '/0')
+		return (s);
 	return (NULL);
 }
