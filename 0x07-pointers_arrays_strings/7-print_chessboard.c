154137bddc754e3110ea8414ce57c1d3c4900689
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-09-29 14:50:39 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,8 +10,9 @@ void print_chessboard(char (*a)[8])
 
 	i = 0;
 	m = 0;
-	for (i = 0, m = 0; a[i][m] != '\0'; i++, m++)
+	for (i = 0, m = 0; a != 0; i++)
 	{
-		_putchar(a[i][m]);
+		for (m = 0; a[i][m] !='\0'; m++)
+			_putchar(a[i][m]);
 	}
 }
