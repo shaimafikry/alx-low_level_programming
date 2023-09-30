44a17383d67cb118fbc88b402edfaea0047677e0
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-09-30 06:13:26 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,6 +6,8 @@
 */
 int _strlen_recursion(char *s)
 {
-	if (s + 1 != 0)
-		return (0 + _strlen_recursion(s + 1));
+	if (*s == 0)
+		return (0);
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
 }
