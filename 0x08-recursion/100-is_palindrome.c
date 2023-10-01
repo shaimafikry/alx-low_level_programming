20b8dbf68b2cd2db923e8167ad71b1019f3493a0
shaimaa <shaimafikry@yahoo.com>
2023-09-30 21:59:53 -0700

    edit

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* _sqrt_recursion - function that returns 1 if a string is a palindrome and 0 if not
+* @s: integer
+* Return: returns 1 if a string is a palindrome and 0 if not
+*/
+int is_palindrome(char *s)
+{
+	
+}
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _sqrt_recursion - function that returns 1 if a string is a palindrome and 0 if not
+* @s1: integer
+* @s2: char
+* Return: returns 1 if a string is a palindrome and 0 if not
+*/
+int wildcmp(char *s1, char *s2)
+{
+	
+}
