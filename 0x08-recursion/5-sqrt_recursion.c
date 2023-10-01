6731959e572bbba5fa03b54b6f6fa8932e2d8179
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-10-02 00:23:37 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,14 +1,28 @@
 #include "main.h"
 /**
+* find_sqrt - act as a loop to find square
+* @n: integer
+* @i: integer that get incermented
+* Return: the natural square root of a number
+*/
+int find_sqrt(int n, int i)
+{
+	if (i * i) == n)
+		return i;
+	else
+{
+	i++;
+	find_sqrt(n, i);
+}
+}
+/**
 * _sqrt_recursion -  returns the natural square root of a number.
 * @n: integer
 * Return: the natural square root of a number
 */
 int _sqrt_recursion(int n)
 {
-	if (n / 2 != 0)
+	if (n <= 0)
 		return (-1);
-	if (n == 1)
-		return (1);
-	return (16 /_sqrt_recursion(n / 2);
+	return (find_sqrt(n, 1);
 }
