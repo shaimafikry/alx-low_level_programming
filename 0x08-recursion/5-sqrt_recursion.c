b87c7af325a86cae95da349024eddb81ddeb0351
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-10-02 00:28:21 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,11 +9,8 @@ int find_sqrt(int n, int i)
 {
 	if (i * i == n)
 		return (i);
-	else
-	{
-		i++;
-		return (find_sqrt(n, i));
-	}
+	i++;
+	return (find_sqrt(n, i));
 }
 /**
 * _sqrt_recursion -  returns the natural square root of a number.
