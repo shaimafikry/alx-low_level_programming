bf7faf00984947a08f3e9952ce1a5e64f44a760c
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-10-02 15:04:50 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,10 +1,31 @@
 #include "main.h"
 /**
-* _sqrt_recursion - function that returns 1 if a string is a palindrome and 0 if not
-* @s: integer
-* Return: returns 1 if a string is a palindrome and 0 if not
+* find_prime - loop to check prime
+* @n: integer
+* @i: integer
+* Return: returns 1 if found 0 if not
 */
-int is_palindrome(char *s)
+int find_prime (int n, int i)
 {
-	
+if (n == i)
+return (1);
+if (n % i == 0)
+return (0);
+i++;
+return (find_prime(n, i));
+}
+/**
+* is_prime_number - function that returns 1 if number is prime and 0 if not
+* @s: integer
+* Return: returns 1 if prime 0.if not
+*/
+int is_prime_number(int n)
+{
+if (n <= 1)
+return (0);
+if (n == 2)
+return (1);
+if (n % 2 == 0)
+return (0);
+return (find_prime(n, 3));
 }
