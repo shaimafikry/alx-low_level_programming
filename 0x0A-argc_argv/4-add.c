287f8463f92e27b287c38f185bbe43d45f451ba3
shaimaa <shaimafikry@yahoo.com>
2023-10-03 23:37:37 +0300

    edit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,21 +18,21 @@ int main(int argc, char **argv)
 		printf("0\n");
 		return (1);
 	}
-	for ( i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (m = 0; argv[i][m] != '\0'; m++)
 		{
-			if (argv[i][m] < 48 || argv[i][m] > 57 )
+			if (argv[i][m] < 48 || argv[i][m] > 57)
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
 	}
-	for (i = 1; i < argc; i ++)
+	for (i = 1; i < argc; i++)
 	{
 		sum += atoi(argv[i]);
-	} 
+	}
 	printf("%d", sum);
 	printf("\n");
 	return (0);
