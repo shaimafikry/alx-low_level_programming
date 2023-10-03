dbb8d8b03287d93b7de635b920dedecdb0803147
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-10-03 10:55:38 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,11 +6,11 @@
  * @argv: string
  * Return: 0 success
  */
-int main (int  __attribute__((__unused__)) argc, char **argv)
+int main(int  __attribute__((__unused__)) argc, char **argv)
 {
 	int m;
 
-	for (m = 0; argv[0][m] !='\0'; m++)
+	for (m = 0; argv[0][m] != '\0'; m++)
 		printf("%c", argv[0][m]);
 	printf("\n");
 	return (0);
