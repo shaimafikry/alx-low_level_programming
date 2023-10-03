0745120ec9ff370bc950384c55f1ad71146c92a2
shaimaa <shaimafikry@yahoo.com>
2023-10-03 01:02:54 -0700

    edit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - entry , prints the number of arg passd to it 
+ * main - entry , prints the number of arg passd to it
  * @argc: count of lines
  * @argv: string
  * Return: 0 success
  */
-
-int main (argc, **argv)
+int main(int argc, __attribute__((unused)) char **argv)
 {
-	printf("%d\n", argc - 1):
+	printf("%d\n", argc - 1);
+	return (0);
 }
