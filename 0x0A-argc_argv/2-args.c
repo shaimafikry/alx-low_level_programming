a040fd4b99e8a0af3a76891da43dd14e9f70a5f3
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-10-03 22:45:58 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main - entry , prints all arg followed by a new line
  * @argc: count of lines
  * @argv: string
  * Return: 0 success
  */
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
         int i;
-        int m;
 
         for (i = 0; i < argc; i++)
 	{
-                for (m = 0; argv[i][m] != '\0'; m++)
-                        printf("%c", argv[i][m]);
+                printf("%s", argv[i]);
 		printf("\n");
 	}
-	printf("\n");
-        return (0);
+	return (0);
 }
