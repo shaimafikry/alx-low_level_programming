3a05d7ce2866ecf9d93325e19e094c8d9128dd64
shaimaa <shaimafikry@yahoo.com>
2023-10-04 23:37:28 +0300

    edit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _strncat - adding n src to dest
  * @dest : a pointer to char
@@ -15,31 +15,38 @@ char *_strncat(char *dest, char *src, int n)
 	count2 = 0, co_dest = 0;
 	for (i = 0; dest[i] != '\0'; i++)
 		co_dest++;
-	i = 0;
-	while (src[i] != '\0')
-	{
+	for (i = 0; src[i] != '\0'; i++)
 		count2++;
-		i++;
-	}
-	i = 0;
 	if (n > count2)
 	{
-		while (i <= count2)
+		for (i = 0; i <= count2; i++)
 		{
 			dest[co_dest] = src[i];
-			i++;
 			co_dest++;
 		}
 	}
-		i = 0;
-		if (n > 0)
-		{
-			while (i < n)
-			{
-			dest[co_dest] = src[i];
-			i++;
-			co_dest++;
-			}
-		}
-		return (dest);
+	for (i = 0; i < n; i++)
+	{
+		dest[co_dest] = src[i];
+		co_dest++;
+	}
+	return (dest);
+}
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strncat(s1, s2, 1);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    printf("%s\n", ptr);
+    ptr = _strncat(s1, s2, 1024);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
 }
