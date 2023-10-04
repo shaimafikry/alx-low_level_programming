4a89710b83e16ddffb09547f0326c8efeb1122a9
shaimaa <shaimafikry@yahoo.com>
2023-10-04 09:01:02 +0300

    edit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * creat_array -  creates array and initialize all of it to a special char
+ * create_array -  creates array and initialize all of it to a special char
  * @size: size of arry
  * @c: special char
  * Return: pointer to an array
