ecbe0a2ed91a955d30098a5a93fc86f1286ac9cc
shaimaa <shaimafikry@yahoo.com>
2023-10-04 09:39:40 +0300

    edit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,8 +1,28 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * creat_array -  creates array and initialize all of it to a special char
- * @size: size of arry
- * @c: special char
+ * _strdup -  creates array and initialize all of it to a special char
+ * @str: string
  * Return: pointer to an array
 */
+char *_strdup(char *str)
+{
+	int i;
+	char *arry;
+	int size;
+
+	if (str == NULL)
+		return (NULL);
+	size = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		size++;
+	arry = malloc(sizeof(char) * size);
+	if (arry == NULL)
+	{
+		free(arry);
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+		arry[i] = str[i];
+	return (arry);
+}
