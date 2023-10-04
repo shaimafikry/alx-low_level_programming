e3bd465d9d8624c210dabe1086450742bd5c99fd
shaimaa <shaimafikry@yahoo.com>
2023-10-04 14:38:15 +0300

    edit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,7 +1,32 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 /**
  * _strdup -  creates array and initialize all of it to a special char
- * @str: string
+ * @width: integer
+ * @height: integer
  * Return: pointer to an array
 */
+int **alloc_grid(int width, int height)
+{
+	int i, m;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+	if (grid == NULL)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+		for (m = 0; m < width; m++)
+			grid[i][m] = 0;
+	return (grid);
+}
