d664bce4d62a33f57efdfa350c42f1fa7a527de0
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-10-11 03:57:43 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 p = malloc(sizeof(dog_t));
 if (p == NULL)
 return (NULL);
-if (owner == NULL || name == NULL || age < 0)
+if (owner == NULL || (name == NULL || age < 0))
 return (NULL);
 p->name = malloc(strlen(name) + 1);
 p->owner = malloc(strlen(owner) + 1);
