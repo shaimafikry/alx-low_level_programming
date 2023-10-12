6659912073004f3cdb1cdf8331b21b981c033bb1
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-10-12 03:21:37 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,28 +7,28 @@
 */
 int main (int argc, char *argv[])
 {
-	int num1, num2, res;
-	char op;
+int num1, num2, res;
+char op;
 
-	op = argv[2][0];
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	if ((op == '/' || op == '%') && num2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
+op = argv[2][0];
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
 if (strlen(argv[2]) > 1)
 {
 printf("Error\n");
 exit(99);
 }
-	res = (*get_op_func(argv[2]))(num1, num2);
-	printf("%d\n", res);
-	return (0);	
+if ((op == '/' || op == '%') && num2 == 0)
+{
+printf("Error\n");
+exit(100);
+}
+res = get_op_func(argv[2])(num1, num2);
+printf("%d\n", res);
+return (0);	
 }
