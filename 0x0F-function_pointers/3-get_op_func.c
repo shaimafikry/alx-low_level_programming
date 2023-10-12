35716bce2b9118c1a21d12fecd2d4a0e7319c037
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-10-12 03:49:03 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,7 +3,7 @@
  * get_op_func -  function pointer selects the correct function to perform the operation asked by the user
  * @s: string
  * @b: integer
- * @a: integer
+ * Return: integer result
 */
 int (*get_op_func(char *s))(int, int)
 {
