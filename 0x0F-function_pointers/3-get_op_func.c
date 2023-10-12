6c2971606f2860492eb00a6a48f3393134e67175
shaimaa <shaimafikry@yahoo.com>
2023-10-12 09:25:18 +0300

    edit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,26 +1,28 @@
 #include "3-calc.h"
 /**
- * get_op_func -  function pointer selects the correct function to perform the operation asked by the user
+ * get_op_func -  function pointer selects the correct function to perform
+ * the operation asked by the user
  * @s: string
- * 
+ * Return: func
 */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
-    i = 0;
-    while (ops[i].op != NULL)
-    {
-	if (s[2] == ops[i].op)
-		return (ops[i].f);
-	i++;
-    }
-    return (NULL);
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op)
+	{
+		if (s[0] == ops[i].op[0])
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,33 +1,35 @@
 #include "3-calc.h"
 /**
  * main - entry point
+ * @argc: count
+ * @argv: strings
  * func to use a calculate
  * Return: sucess if 0
 */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int num1, num2, res;
-	char op;
+	char *op;
 
-	op = argv[2][0];
-	if (argc != 4 )
+	op = argv[2];
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (op != '+' && (op != '-' && (op != '*' && (op != '/' && op != '%'))))
+	if (op[1] != '\0')
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	if ((op == '/' || op == '%') && num2 == 0)
+	if ((*op == '/' || *op == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	res = (*get_op_func(argv[2]))(num1,num2);
+	res = (*get_op_func(op))(num1, num2);
 	printf("%d\n", res);
-	return (0);	
+	return (0);
 }
