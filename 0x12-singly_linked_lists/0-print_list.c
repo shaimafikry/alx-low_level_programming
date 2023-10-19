e126afd797c42a6715a99177be2b0e97bbbe993e
shaimaa <shaimafikry@yahoo.com>
2023-10-20 01:51:36 +0300

    edit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,34 +7,20 @@
 size_t print_list(const list_t *h)
 {
 	int i;
-	list_t *m;
-
 	i = 0;
 	if (h == NULL)
 	{
 		return (i);
 	}
-	m = malloc(sizeof(list_t));
-	if (m == NULL)
-		return (-1);
-	m->str = malloc(sizeof(h->str));
-	if (m->str == NULL)
+	while (h != NULL)
 	{
-		free(m);
-		return (-1);
-	}
-	m->str = h->str;
-	m->len = h->len;
-	m->next = h->next;
-	while (m != NULL)
-	{
-		if (m->str == NULL)
+		if (h->str == NULL)
 		{
-			m->str = "(nil)";
-			m->len = 0;
+			printf("[0] (nil)\n");
 		}
-		printf("[%d] %s\n", m->len, m->str);
-		m = m->next;
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
 		i++;
 	}
 	return (i);
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,5 +8,16 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *n;
 	if (head == NULL)
 		return (-1);
-		
+	n = malloc(sizeof(list_t));
+	if (n == NULL)
+		return (NULL);
+	n->str = malloc(sizeof(char *));
+	if (n->str == NULL)
+	{
+		free(n);
+		return (NULL);
+	}
+	n->str = strdub(str);
+	head-> next = n->next;
+	return (n);
 }
