0789885437727856a66e50321282579cdd847a1c
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-10-21 09:26:18 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,11 +15,6 @@ char *_strncpy(char *dest, char *src, int n)
 	count = 0;
 	for (i = 0; src[i] != '\0'; i++)
 		count++;
-	if (src[0] == '\0')
-	{
-		dest[0] = '\0';
-		return (dest);
-	}
 	if (n > count)
 	{
 		for (i = 0; src[i] != '\0'; i++)
