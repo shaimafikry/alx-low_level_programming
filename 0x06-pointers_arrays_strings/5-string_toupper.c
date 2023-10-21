b9f0f19f16f27630d049fb273b2682509499a85f
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-10-21 11:51:51 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,21 @@
 #include "main.h"
 
 /**
- * _strcpy - copy strings src to dest
- * @dest : a pointer to char
- * @src  : a pointer to char
- * Return: return pointer to dest
+ * string_toupper - copy strings src to dest
+ * @char * : a pointer to char
+ * Return: return pointer to char
  */
+char *string_toupper(char *)
+{
+	char *str;
 
+	if (str == NULL)
+		return (-1);
+	while (str != 0)
+	{
+		if (str >= 97 && str <= 122)
+			str = str - 12;
+		str++;
+	}
+	return (str);
+}
