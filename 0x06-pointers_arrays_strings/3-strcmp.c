82d084bb58585aa91182f2895045eff1901a3b39
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-10-21 10:04:45 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,7 @@ int _strcmp(char *s1, char *s2)
 	int i;
 
 	i = 0;
-	for (; *s1 != '\0' && *s2 != '\0'; s2++, ,s1++, i++)
+	for (; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] > s2[i])
 			return (s1[i] - s2[i]);
