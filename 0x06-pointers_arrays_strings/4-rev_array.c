6cb66cd20326b0c47827e56b7badf8da66bfa63e
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-10-21 10:09:30 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,8 +2,27 @@
 #include "main.h"
 
 /**
- * _strcpy - copy strings src to dest
- * @dest : a pointer to char
- * @src  : a pointer to char
- * Return: return pointer to dest
+ * reverse_array - print reversed array
+ * @a : a pointer to array
+ * @n  : array num
+ * Return: no return
  */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int r[n];
+
+	i = 0;
+	while (n > i)
+	{
+		r[i] = a[n];
+		i++;
+	}
+	i = 0;
+	while (i < n)
+	{
+		a[i] = r[i];
+		i++;
+	}
+	return (a);
+}
