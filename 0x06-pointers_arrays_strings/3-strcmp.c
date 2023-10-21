cd2019fb0289475940f003f321a2ff23fde775fb
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-10-21 09:51:42 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,21 @@
-
 #include "main.h"
-
 /**
- * _strcpy - copy strings src to dest
- * @dest : a pointer to char
- * @src  : a pointer to char
- * Return: return pointer to dest
+ * _strcmp - copy strings src to dest
+ * @s1: a pointer to char
+ * @s2: a pointer to char
+ * Return: num
  */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	for (;*s1 !='\0' && *s2 != '\0';i++)
+	{
+		if (s1[i] > s2[i])
+			return (i);
+		if (s1[i] < s2[i])
+			return (-i);
+	}
+	return (0);
+}
