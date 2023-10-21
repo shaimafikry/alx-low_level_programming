949f289b704defec5293441d50e28e18766db284
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-10-21 08:55:53 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -22,9 +22,12 @@ char *_strncpy(char *dest, char *src, int n)
 			dest[i] = src[i];
 		}
 	}
+	else
+	{
 	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
+	}
 	}
 	dest[i] = '\0';
 	return (dest);
