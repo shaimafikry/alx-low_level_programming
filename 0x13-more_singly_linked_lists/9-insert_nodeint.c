aa160e86c744571412640ab34d0aea60936c6280
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2023-10-24 13:07:32 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	ptr = *head;
-if (*head == NULL)
+if (*head == NULL || idx == 0)
 	{
 		new->next = (*head);
 		(*head) = new;
