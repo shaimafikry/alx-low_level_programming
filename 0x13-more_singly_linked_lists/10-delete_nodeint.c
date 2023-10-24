713581b813bb9fa24343e2ef3e43c6d65793df3b
shaimaa <shaimafikry@yahoo.com>
2023-10-24 23:15:10 +0300

    edit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		{
 			while (i < index)
 			{
-				if (current != NULL && prev != NULL)
+				if (current != NULL || prev != NULL)
 				{
 					prev = current;
 					current = current->next;
