d96cc28ccdbfd57410ce9a157ea3aa4daeb454f8
shaimaa <shaimafikry@yahoo.com>
2023-10-24 04:44:45 +0300

    delete test files

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	ptr = *head;
 	if (idx == 0)
 	{
-		new->next = (*head);
+		new->next = (*head)->next;
 		(*head) = new;
 		return (new);
 	}
