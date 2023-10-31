05d0ca517d51236bdb3d0f236a34df0db501e9b7
shaimaa <shaimafikry@yahoo.com>
2023-10-31 15:04:59 +0200

    add file task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,23 +1,25 @@
 #include "main.h"
 /**
- * 
-*/
+ * read_textfile - reads a text file and prints it to the POSIX std output
+ * @filename: name of the file to read
+ * @letters: num of letters to read and print
+ * Return: actual number of letters it could read and print, or 0 on failure
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *f =  fopen(filename, "r");
 	size_t i;
+	char c = fgetc(f);
 
 	i = 0;
 	if (f == NULL)
 		return (0);
-	do
-	{
-		char c = fgetc(f);
+	do {
 		if (feof(f))
 		{
 			break;
 		}
-		printf("%c",c);
+		printf("%c", c);
 		i++;
 	} while (i < letters);
 
