a83ad07f2e1177b1903bc1a405659ea34021cacb
shaimaa <shaimafikry@yahoo.com>
2023-10-31 15:07:21 +0200

    edit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,20 +9,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *f =  fopen(filename, "r");
 	size_t i;
-	char c = fgetc(f);
+	char c;
 
 	i = 0;
 	if (f == NULL)
 		return (0);
-	do {
+	while (i < letters)
+	{
+		c = fgetc(f);
 		if (feof(f))
 		{
 			break;
 		}
 		printf("%c", c);
 		i++;
-	} while (i < letters);
-
+	}
 	fclose(f);
 	return (i);
 }
