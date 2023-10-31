0b96dacb18feb047cc8ce01efcb4d5487506d24a
shaimaa <shaimafikry@yahoo.com>
2023-10-31 18:06:17 +0200

    edit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * read_textfile - function creates a file
+ * create_file - function creates a file
  * @filename: name of the file to read
  * @text_content: num of letters to read and print
  * Return: actual number of letters it could read and print, or 0 on failure
  */
 int create_file(const char *filename, char *text_content)
- {
+{
 	FILE *f = fopen(filename, "w");
-	
+
 	if (f == NULL  || filename == NULL)
 		return (-1);
-	fprintf(f, "%s", text_content);
+	if (text_content != NULL)
+		fprintf(f, "%s", text_content);
 	chmod(filename, 0600);
 	fclose(f);
 	return (1);
- }
+}
