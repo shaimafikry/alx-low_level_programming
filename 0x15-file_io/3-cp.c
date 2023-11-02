f4af4f78d862830359c0a76098cc4158a18388f7
shaimaa <shaimafikry@yahoo.com>
2023-11-02 13:52:47 +0200

    edit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,17 +13,17 @@ int main(int argc, char *argv[])
 	char *file1 = argv[1], *file2 = argv[2];
 	int from = open(file1, O_RDONLY), to = open(file2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
-	if (argc != 3)
+	if (argc != 3 || (file1 == NULL || file2 == NULL))
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (from == -1 || file1 == NULL)
+	if (from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s", file1);
 		exit(98);
 	}
-	if (to == -1 || file2 == NULL)
+	if (to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file2);
 		exit(99);
