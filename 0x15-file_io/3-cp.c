cbbdff0fc47fdb56afd6650ac7cf69ec6e934762
shaimaa <shaimafikry@yahoo.com>
2023-11-02 14:14:48 +0200

    edit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -37,11 +37,6 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-	if (b_rd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file1);
-		exit(98);
-	}
 	if (close(from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
