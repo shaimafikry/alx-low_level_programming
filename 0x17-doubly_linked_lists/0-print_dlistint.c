364e8b27f1faf256ca05a48d3d93993435b3c5b4
shaimaa <shaimafikry@yahoo.com>
2023-12-28 19:44:53 +0200

    edit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * print_dilistint - prints list
+ * print_dlistint - prints list
  * @h: pointer to the head
  * Return: num of list elemnts
 */
@@ -10,14 +10,13 @@ size_t print_dlistint(const dlistint_t *h)
 
 	if (h == NULL)
 		return (0);
-	
+
 	while (h != NULL)
 	{
-		printf("%d\n",h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
 
 	return (i);
-	
 }
