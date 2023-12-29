4b99eee20671cee776e1318decc9a281b6138eca
shaimaa <shaimafikry@yahoo.com>
2023-12-29 17:30:22 +0200

    edit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,10 +38,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			current->prev = new;
 			return (new);
 		}
-
 		current = current->next;
 		i++;
 	}
+	if (idx == i)
+	{
+		new->next = NULL;
+		current->next = new;
+		new->prev = current;
+		return (new);
+	}
+
 		free(new);
 		return (NULL);
 }
