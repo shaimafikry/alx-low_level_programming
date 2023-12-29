4123a825c9ed2f40887c1faa84bab53d936b6994
shaimaa <shaimafikry@yahoo.com>
2023-12-29 13:03:51 +0200

    edit

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index -  get the sum of nodes
+ * @head: list
+ * Return: sum
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+	unsigned int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	current = head;
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+}
