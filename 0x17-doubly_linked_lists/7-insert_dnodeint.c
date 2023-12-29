25f59166be2aea8496398a984aa013a179dc1d4e
shaimaa <shaimafikry@yahoo.com>
2023-12-29 16:45:53 +0200

    edit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new);
 	}
 	current = (*h);
-	while (current != NULL || i < idx)
+	while (current != NULL && i < idx)
 	{
 		current = current->next;
 		i++;
