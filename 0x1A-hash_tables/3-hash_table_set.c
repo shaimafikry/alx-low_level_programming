2d1299ba29b206aed40a6c6d5e59aafcfa0f36f2
shaimaa <shaimafikry@yahoo.com>
2023-12-31 16:24:55 +0200

    edit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,33 +10,31 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_table_t *table;
 	hash_node_t *item;
-	hash_node_t *new;
+	hash_node_t *head;
 	unsigned int index;
-	unsigned int i ;
 
 	/*handling error*/
 	if (ht == NULL || value == NULL)
 		return (0);
 	/*memroy allocation*/
-	item->key = malloc (strlen(key));
-	item->value = malloc(strlen(value));
+	item = malloc (sizeof(hash_node_t));
+	item->key = malloc (strlen(key)+ 1);
+	item->value = malloc(strlen(value) + 1);
 	if (!item->key || !item->value)
 		return (0);
 	table = ht;
-	new = item;
-	strdup(value,item->value);/*duplicate the value*/
+	item->key =  key;
+	item->value = strdup(value);/*duplicate the value*/
 	index = key_index(key, table->size);
-	for (i = 0; i < table->size; i++)
-	{
-		if (i == index)
-		{
-			new->next = item;
-			item = new;
-			table->array[i] = item;
-		}
-		else
-			table->array[i] = item;
-	}
 
+	head = table->array[index];/*to check if the index is empty*/
+	if (head == NULL)
+			table->array[index] = item;
+	else
+	{
+		item->next = head;
+		head = item;
+		table->array[index] = item;
+	}
 	return (1);
 }
