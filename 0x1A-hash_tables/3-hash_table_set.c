8d1f157a47b9f6f0f46b549d62fdfd70520649dd
shaimaa <shaimafikry@yahoo.com>
2024-01-01 21:30:35 +0200

    edit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!item->key || !item->value)
 		return (0);
 	table = ht;
-	item->key =  strdup(key);
+	item->key =  key;
 	item->value = strdup(value);/*duplicate the value*/
 	index = key_index((const unsigned char*)key, table->size);
 
@@ -34,6 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		item->next = head;
 		head = item;
+		table->array[index] = item;
 	}
 	return (1);
 }
