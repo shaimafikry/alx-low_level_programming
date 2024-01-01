837541b5198040bf71c604bd41f6ee7024479522
shaimaa <shaimafikry@yahoo.com>
2024-01-01 22:42:17 +0200

    edit

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,6 +1,25 @@
 #include "hash_tables.h"
 /**
- * 
- * 
- * 
+ * hash_table_get - function that retrieves a value associated with a key.
+ * @ht: hash table
+ * @key: key
+ * Return: 1 on success 0 if not
 */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+unsigned int index;
+char *value;
+
+/*handling error*/
+if (ht == NULL || key == NULL)
+	return (NULL);
+/*memroy allocation*/
+index = key_index((const unsigned char *)key, ht->size);
+if (!ht->array[index])
+	return (NULL);
+value = malloc(sizeof(ht->array[index]->value));
+	if (!value)
+		return (NULL);
+value = ht->array[index]->value;
+return (value);
+}
