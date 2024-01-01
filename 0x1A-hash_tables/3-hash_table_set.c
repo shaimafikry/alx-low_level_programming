83f2834e55534e53407d4f85c0d8ddaf4cf91634
shaimaa <shaimafikry@yahoo.com>
2024-01-01 22:01:40 +0200

    edit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,33 +8,35 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_table_t *table;
-	hash_node_t *item;
-	hash_node_t *head;
-	unsigned int index;
+hash_table_t *table;
+hash_node_t *item;
+hash_node_t *head;
+unsigned int index;
 
-	/*handling error*/
-	if (ht == NULL || value == NULL)
-		return (0);
-	/*memroy allocation*/
-	item = malloc (sizeof(hash_node_t));
-	item->key = malloc (strlen(key)+ 1);
-	item->value = malloc(strlen(value) + 1);
-	if (!item->key || !item->value)
-		return (0);
-	table = ht;
-	item->key =  strdup(key);
-	item->value = strdup(value);/*duplicate the value*/
-	item->next = NULL;
-	index = key_index((const unsigned char*)key, table->size);
+/*handling error*/
+if (ht == NULL || value == NULL)
+	return (0);
+/*memroy allocation*/
+item = malloc (sizeof(hash_node_t));
+item->key = malloc (strlen(key)+ 1);
+item->value = malloc(strlen(value) + 1);
+if (!item->key || !item->value)
+	return (0);
+table = ht;
+item->key =  strdup(key);
+item->value = strdup(value);/*duplicate the value*/
+item->next = NULL;
+index = key_index((const unsigned char*)key, table->size);
 
-	head = table->array[index];/*to check if the index is empty*/
-	if (head == NULL)
-			table->array[index] = item;
-	else
-	{
-		table->array[index] = item; 
-		item->next = head;
-	}
-	return (1);
+head = table->array[index];/*to check if the index is empty*/
+if (head == NULL)
+{
+		table->array[index] = item;
+}
+else
+{
+	table->array[index] = item; 
+	item->next = head;
+}
+return (1);
 }
