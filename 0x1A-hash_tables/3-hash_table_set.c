7d48ac421e9a4e6257eefb9ead8bd24c050885ad
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2024-01-03 22:54:39 +0200

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,7 +27,7 @@ if (current != NULL)
 {
 	if (strcmp(item->key, current->key) == 0)
 	{
-		current->value = realloc(current->value, strlen(value) + 1);
+		free(current->valu)e;
 		current->value = strdup(value);
 		return (1);
 	}
