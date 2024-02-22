87a224bab9f6b1ed22febad7d1ab6f7f0dcd5252
shaimaa <shaimafikry@yahoo.com>
2024-02-23 01:10:37 +0200

    edit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,27 +6,19 @@ def island_perimeter(grid):
     """ returns the perimeter of the island described in grid"""
 
     area = 0
+    count = 0
     """grid length and width doesnt exced 100"""
     if len(grid) == 100 or (i for i in grid) == 100:
         return 0
     for i in range(len(grid)):
         for m in range(len(grid[i])):
             if grid[i][m] == 1:
-                area += 1
-                if grid[i - 1][m] == 0:
-                     area += 1
-                if grid[i][m - 1] == 0:
-                     area += 1
-                if m + 1 < len(grid[i]):
-                     if grid [i][m + 1] == 0:
-                         area += 1
-                else:
-                     area += 1
-                if i + 1 < len(grid):
-                    if grid[i + 1][m] == 0:
-                         area += 1
-                else:
-                     area += 1
+                count += 1
+        area += count * 4 - ((count - 1) * 2)
+        if i != 0:
+            area - count * 2
+        count = 0
+
     return area
 
 
