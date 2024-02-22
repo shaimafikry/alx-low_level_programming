088c03d0ed80e2500583f8a01bb260c2fb6e2502
shaimaa <shaimafikry@yahoo.com>
2024-02-22 22:38:18 +0200

    edit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,30 +1,45 @@
 #!/usr/bin/python3
-"""
- grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
- """
-          
+"""module for porblem solving """
+
+
 def island_perimeter(grid):
     """ returns the perimeter of the island described in grid"""
+
     area = 0
+    """grid length and width doesnt exced 100"""
+    if len(grid) == 100 or (i for i in grid) == 100:
+        return 0
     for i in range(len(grid)):
         for m in range(len(grid[i])):
             if grid[i][m] == 1:
-                if grid[i-1][m] == 0:
+                if grid[i - 1][m] == 0:
                     area += 1
-                if grid[i][m-1] == 0:
+                if grid[i][m - 1] == 0:
                     area += 1
-                if grid[i][m+1] == 0:
-                    area += 1
-                if i+1 < len(grid):
-                    if grid[i+1][m] == 0:
+                if m + 1 < len(grid[i]):
+                    if grid[i][m + 1] == 0:
                         area += 1
-                else:
-                    return 0 
-                    
+                if i + 1 < len(grid):
+                    if grid[i + 1][m] == 0:
+                        area += 1
+
     return area
+
+
+# grid = [
+#     [0, 1, 0, 0, 0, 1],
+#     [1, 1, 0, 0, 0, 1],
+#     [1, 1, 0, 1, 1, 1],
+#     [0, 1, 1, 1, 0, 0],
+#     [0, 0, 1, 1, 0, 0],
+# ]
+# """[ [0, 0, 0, 0, 0, 0],
+#      [0, 1, 1, 1, 0, 0],
+#      [0, 1, 1, 1, 0, 0],
+#      [0, 1, 1, 1, 0, 0]]"""
+# """ [[0, 0, 0, 0, 0, 0],
+#         [0, 1, 0, 0, 0, 0],
+#         [0, 1, 0, 0, 0, 0],
+#         [0, 1, 1, 1, 0, 0],
+#         [0, 0, 0, 0, 0, 0] ]"""
+# print(island_perimeter(grid))
