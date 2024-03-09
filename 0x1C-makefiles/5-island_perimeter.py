06f778f31e290e9c5714b16cba71f34bcbd079d0
shaimaa <shaimafikry@yahoo.com>
2024-03-09 12:30:41 +0200

    edit 5 file

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,27 +5,41 @@
 def island_perimeter(grid):
     """ returns the perimeter of the island described in grid"""
 
+    grid_count = len(grid)
     area = 0
-    count = 0
+    flag = 0
+    edge = 0
+    side = 0
     """grid length and width doesnt exced 100"""
-    if len(grid) == 100 or (i for i in grid) == 100:
+    if grid_count > 100:
         return 0
-    for i in range(len(grid)):
+    for i in grid:
+        if len(i) > 100:
+            return 0
+    for i in range(grid_count):
+        # to loop in one lst
         for m in range(len(grid[i])):
             if grid[i][m] == 1:
-                count += 1
-        area += count * 4 - ((count - 1) * 2)
-        if i != 0:
-            area - count * 2
-        count = 0
-
+                if i == 0 or i == grid_count - 1:
+                    flag = 1  # to mark first piece of land
+                if flag == 1:
+                    edge += 3
+                    if m < (len(grid[i]) - 1) and grid[i][m + 1] == 1:
+                        edge -= 2
+                else:
+                    side += 2
+                    if m < (len(grid[i]) - 1) and grid[i][m + 1] == 1:
+                        side -= 1
+        flag = 0
+        area += edge + side
+        edge = 0
+        side = 0
+        # print("count after fist list", area)
     return area
 
-#  grid = [
-#         [0, 0, 0, 0, 0, 0],
-#         [0, 1, 0, 0, 0, 0],
-#         [0, 1, 0, 0, 0, 0],
-#         [0, 1, 1, 1, 0, 0],
-#         [0, 0, 0, 0, 0, 0]
-#     ]
-#     print(island_perimeter(grid))
+
+# grid = [[1, 1, 0, 1, 0, 0],
+#         [0, 1, 0, 1, 0, 0],
+#         [0, 1, 0, 1, 0, 0]
+#         ]
+# print(island_perimeter(grid))
