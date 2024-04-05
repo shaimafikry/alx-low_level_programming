e32aeafd68af7d4489154d55d9d1f95ed880c11f
shaimaa <shaimafikry@yahoo.com>
2024-04-05 10:44:07 +0200

    7-leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,12 +1,27 @@
 #include "main.h"
-vi 
 /**
- * _strcpy - copy strings src to dest
- * @dest : a pointer to char
- * @src  : a pointer to char
+ * _leet - function that encodes a string into 1337.
+ * @str : a pointer to char
  * Return: return pointer to dest
  */
 
-char _strcmp(char *s1, char *s2);
-
-
+char *leet(char *str)
+{
+	int i = 0, m = 0;
+	char *let = "44007733";
+	char *cr = "aAoOtTeE";
+	while (str[i] != '\0')
+	{
+		m = 0;
+		while (cr[m] != '\0')
+		{
+			if (str[i] == cr[m])
+			{
+				str[i] = let[m];
+			}
+			m++;
+		}
+		i++;
+	}
+	return (str);
+}
