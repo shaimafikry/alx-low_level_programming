f7aed28ff782803f93725225334dc5aeb17d92c1
shaimaa <shaimafikry@yahoo.com>
2024-04-05 10:19:30 +0200

    6-cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,32 @@
 #include "main.h"
 
 /**
- * _strcpy - copy strings src to dest
- * @dest : a pointer to char
- * @src  : a pointer to char
- * Return: return pointer to dest
+ * cap_string - converts lower to cpas
+ * @str : a pointer to char
+ * Return: return pointer to new one
  */
 
+char *cap_string(char *str)
+{
+	int i = 0, m = 0;
+	char *word_sep = " \t\n,;.!?{}()\"";
+
+	while (str[i] != '\0')
+	{
+		m = 0;
+		while (word_sep[m] != '\0')
+		{
+			if (str[i] == word_sep[m])
+			{
+				i++;
+				if (str[i] >= 97 && str[i] <= 122)
+					str[i] -= 32;
+				else
+					i--;
+			}
+			m++;
+		}
+		i++;
+	}
+return (str);
+}
