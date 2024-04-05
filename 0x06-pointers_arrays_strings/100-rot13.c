1c305232506ac049fe6547b1f310dd17223380d6
shaimaa <shaimafikry@yahoo.com>
2024-04-05 23:20:32 +0200

    100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,11 +1,26 @@
 #include "main.h"
 
 /**
- * _strcpy - copy strings src to dest
- * @dest : a pointer to char
- * @src  : a pointer to char
+ * rot13 - encodes a string using rot13.
+ * @str : a pointer to char
  * Return: return pointer to dest
  */
 
-*_strcat(char *dest, char *src)
+char *rot13(char *str)
+{
+	int i = 0;
 
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'm')
+		str[i] += 13;
+		else if (str[i] >= 'A' && str[i] <= 'M')
+		str[i] += 13;
+		else if (str[i] >= 'n' && str[i] <= 'z')
+		str[i] -= 13;
+		else if (str[i] >= 'N' && str[i] <= 'Z')
+		str[i] -= 13;
+		i++;
+	}
+	return (str);
+}
