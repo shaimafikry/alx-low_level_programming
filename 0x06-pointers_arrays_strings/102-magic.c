604034bfc83ca545c32948b8c4f1eb90b916c3cf
shaimaa <shaimafikry@yahoo.com>
2024-04-06 02:54:53 +0200

    102

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * _strcpy - copy strings src to dest
- * @dest : a pointer to char
- * @src  : a pointer to char
- * Return: return pointer to dest
- */
+int main(void)
+{
+  int n;
+  int a[5];
+  int *p;
 
-*_strcat(char *dest, char *src)
-
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  n = a[2];
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
+}
